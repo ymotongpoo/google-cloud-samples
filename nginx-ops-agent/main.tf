06a2ecd58fa8c4f599228a80373ca066ed7cb083
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2022-01-17 17:04:38 +0900

    add terraform script and cloud-init stub

---
+++ nginx-ops-agent/main.tf
@@ -0,0 +1,136 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("${var.credentaial_key_path}")
+}
+
+provider "random" {}
+
+variable "project_id" {
+  type        = string
+  description = "Google Cloud Project ID"
+  default     = "OVERRIDE_DEFAULT_VALUE_WITH_YOUR_PROJECT_ID"
+}
+
+variable "credentaial_key_path" {
+  type        = string
+  description = "Path to the JSON key file for the Google Cloud service account"
+  default     = "OVERRIDE_DEFAULT_VALUE_WITH_YOUR_CREDENTIAL_KEY_PATH"
+}
+
+variable "zone" {
+  type        = string
+  description = "Default compute zone in Google Cloud"
+  default     = "us-central1-a"
+}
+
+locals {
+  required_services = [
+    "iam",
+    "compute",
+    "logging",
+    "monitoring",
+  ]
+
+  required_roles = [
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/compute.osLogin",
+  ]
+}
+
+resource "random_pet" "machine_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "google_service_account" "default" {
+  project      = var.project_id
+  account_id   = "nginx-ops-agent-sample"
+  display_name = "Nginx + Ops Agent Sample"
+}
+
+resource "google_project_service" "required_service" {
+  project            = var.project_id
+  for_each           = toset(local.enable_services)
+  service            = "${each.value}.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_iam_member" "default" {
+  project    = var.project_id
+  for_each   = toset(local.required_roles)
+  role       = each.value
+  member     = "serviceAccount:${google_service_account.default.email}"
+  depends_on = [google_project_service.required_service]
+}
+
+resource "google_compute_instance" "default" {
+  project      = var.project_id
+  name         = random_pet.machine_name.id
+  machine_type = "e2-medium"
+  zone         = var.zone
+  tags         = ["yoshifumi-sample"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  metadata = {
+    google-logging-enabled = "true"
+    user-data              = file("./cloud-init.yaml")
+  }
+  service_account {
+    email = google_service_account.default.email
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+  depends_on = [null_resource.container]
+}
+
